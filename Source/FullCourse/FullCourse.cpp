6d4f74159ea61760a3daefe386e160d98f0eaa5e
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2023-11-02 12:51:29 +0300

    Init project

---
+++ Source/FullCourse/FullCourse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FullCourse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FullCourse, "FullCourse" );
