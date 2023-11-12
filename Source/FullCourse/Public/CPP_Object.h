da3ebcbb47dbaa5cd54e93d97dfe8b21a5cd8426
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2023-11-12 17:07:35 +0300

    Done basics

---
+++ Source/FullCourse/Public/CPP_Object.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CPP_Object.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FULLCOURSE_API UCPP_Object : public UObject
+{
+	GENERATED_BODY()
+	
+};
