7704b316188240f0a810843b2ca66688ca5fac6f
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2023-11-19 20:51:11 +0300

    TPL contunie
    Time  7:35:26

---
+++ Plugins/TPL_TestPlugin/Source/ThirdParty/TPL_TestPluginLibrary/TPL_TestPluginLibrary.Build.cs
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class TPL_TestPluginLibrary : ModuleRules
+{
+	public TPL_TestPluginLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+		PublicSystemIncludePaths.Add("$(ModuleDir)/Public");
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			// Add the import library
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "ExampleLibrary.lib"));
+
+			// Delay-load the DLL, so we can load it from the right place first
+			PublicDelayLoadDLLs.Add("ExampleLibrary.dll");
+
+			// Ensure that the DLL is staged along with the executable
+			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/TPL_TestPluginLibrary/Win64/ExampleLibrary.dll");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libExampleLibrary.dylib"));
+			RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/TPL_TestPluginLibrary/Mac/Release/libExampleLibrary.dylib");
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Linux)
+		{
+			string ExampleSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", "TPL_TestPluginLibrary", "Linux", "x86_64-unknown-linux-gnu", "libExampleLibrary.so");
+			PublicAdditionalLibraries.Add(ExampleSoPath);
+			PublicDelayLoadDLLs.Add(ExampleSoPath);
+			RuntimeDependencies.Add(ExampleSoPath);
+		}
+	}
+}
