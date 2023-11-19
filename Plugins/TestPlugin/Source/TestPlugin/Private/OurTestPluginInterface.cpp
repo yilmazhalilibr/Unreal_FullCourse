2127023f369f8ae37a5d983788f9d2fdd4effd1f
Halil <77546808+yilmazhalilibr@users.noreply.github.com>
2023-11-19 19:25:29 +0300

    Created plugin

---
+++ Plugins/TestPlugin/Source/TestPlugin/Private/OurTestPluginInterface.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OurTestPluginInterface.h"
+
+// Add default functionality here for any IOurTestPluginInterface functions that are not pure virtual.
+
+
